94ca646c94fc75402116770aa9802d8951352f06
Robert2703-ops <robertpereira347@gmail.com>
2021-05-03 17:20:11 -0300

    first commmit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,115 @@
 import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
+
 export class HomePage {
 
-  constructor() {}
+  public salary : number;
+  public dependents: number;
+  private discounts:number;
+  public inss:number;
+  public irrf:number;
+  public liquid_salary:number;
 
+  test()
+  { 
+    this.dependents = this.dependents > 0 ? this.dependents : 0;
+    let values;
+
+    switch (true)
+    {
+      case this.salary <= 1751: 
+        this.inss = this.salary * 0.08;
+        this.discounts = this.salary - this.dependents - this.inss;
+
+        values = this.irrf_calc(this.discounts)
+
+        this.irrf = values[0] - values[1]
+
+        this.liquid_salary = this.salary - (this.irrf + this.inss)
+        break
+
+      case this.salary <= 2979:
+        this.inss = this.salary * 0.09;
+        this.discounts = this.salary - this.dependents - this.inss;
+
+        values = this.irrf_calc(this.discounts)
+
+        this.irrf = values[0] - values[1]
+        
+        this.liquid_salary = this.salary - (this.irrf + this.inss)
+        break
+
+      case this.salary <= 5839:
+        this.inss = this.salary * 0.11;
+        this.discounts = this.salary - this.dependents - this.inss;
+
+        values = this.irrf_calc(this.discounts)
+
+        this.irrf = values[0] - values[1]
+        
+        this.liquid_salary = this.salary - (this.irrf + this.inss)
+        break
+
+      case this.salary >= 5840:
+        this.inss = 642
+        this.discounts = this.salary - this.dependents - this.inss;
+
+        values = this.irrf_calc(this.discounts)
+
+        this.irrf = values[0] - values[1]
+        
+        this.liquid_salary = this.salary - (this.irrf + this.inss)
+        break
+    }
+  }
+
+  private irrf_calc(value : number)
+  {
+    switch(true)
+    {
+      case value <= 1902:
+        return [0, 0]
+
+      case value >= 1903 && value <= 2826:
+        return [value * 0.075 , 142]
+        break
+
+      case value <= 3751:
+        return [value * 0.15 , 354]
+        break
+
+      case value <= 4664:
+        return [value * 0.225 , 636]
+        break
+
+      case value > 4664:
+        return [value * 0.275 , 869]
+        break
+    }
+  }
+
+  public show_salary() 
+  {
+    return this.salary;
+  }
+  public show_inss()
+  {
+    return this.inss;
+  }
+  public show_irrf()
+  {
+    return this.irrf;
+  }
+  public show_liquidsalary()
+  {
+    return this.liquid_salary;
+  }
 }
